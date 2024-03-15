41f6582db9fce764dbdd4c5e48890cb3c8567097
kulde <ku1de@icloud.com>
2024-03-15 18:47:52 +0100

    router with defined routes created

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,12 @@
 import express from "express";
+import authRoutes from "./routes/auth.routes";
+import userRoutes from "./routes/users.routes";
+
 
 const router = express.Router();
+
+
+router.use("/api/auth", authRoutes);
+router.use("/api/users", userRoutes);
 
 export default router;
