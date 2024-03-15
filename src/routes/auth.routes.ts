dc0bbe8de52c086f9154d6d545a38bd43068808a
kulde <ku1de@icloud.com>
2024-03-15 18:47:38 +0100

    authUser routes defined

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { AuthController } from "../controllers/AuthController";
+import { auth } from "../middlewares/auth";
+
+const router = express.Router();
+const authController = new AuthController();
+
+router.post("/register", authController.register);
+
+router.post("/login", authController.login);
+
+// router.post("/logout", authController.logout);
+
+router.get("/profile", auth, authController.getProfile)
+
+router.put("/profile", auth, authController.updateProfile)
+
+export default router;
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { UserController } from "../controllers/UserController";
+import { auth } from "../middlewares/auth";
+import { isAdmin } from "../middlewares/isAdmin";
+
+const router = express.Router();
+const userController = new UserController();
+
+router.get("/", auth, isAdmin, userController.getAll);
+
+router.get("/:id", auth, isAdmin, userController.getById);
+
+router.post("/", auth, isAdmin, userController.create);
+
+router.patch("/:id", auth, isAdmin, userController.update);
+
+router.delete("/:id", auth, isAdmin, userController.delete);
+
+export default router;
