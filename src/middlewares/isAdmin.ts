8aa0a97730ae266a4577f7dce61c3cd31f540856
kulde <ku1de@icloud.com>
2024-03-15 18:50:19 +0100

    admin middleware finished

---
+++ src/middlewares/isAdmin.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+
+export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+	const roles = req.tokenData.userRoles;
+
+	if (!roles.includes("admin")) {
+		return res.status(StatusCodes.FORBIDDEN).json({
+			message: "You are not allowed to access this resource",
+		});
+	}
+
+	next();
+};
