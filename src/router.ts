dbadcf7e3834f49ac7e61522a7a803877521a517
kulde <ku1de@icloud.com>
2024-04-07 21:20:01 +0200

    Add Posts and Songs routes to Router

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes";
 import userRoutes from "./routes/users.routes";
+import postRoutes from "./routes/posts.routes";
+import songRoutes from "./routes/songs.routes";
 
 
 const router = express.Router();
@@ -8,5 +10,7 @@ const router = express.Router();
 
 router.use("/api/auth", authRoutes);
 router.use("/api/users", userRoutes);
+router.use("/api/posts", postRoutes);	
+router.use("/api/songs", songRoutes);
 
 export default router;
